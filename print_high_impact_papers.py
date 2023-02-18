f023d7e79d2cb8e978c350be71fd90d336c01bcf
WenqiJiang <wenqi.j@columbia.edu>
2023-02-18 08:09:57 +0800

    finish SIGMOD

--- print_high_impact_papers.py
+++ print_high_impact_papers.py
@@ -3,7 +3,9 @@ Return papers that has at least min(cite_per_year * years, max_citation_threshol
 
 Example usage:
 
-python3 print_high_impact_papers.py --citation_json_path 'citation_json/OSDI.json' --cite_per_year 30 --max_citation_threshold 500
+python3 print_high_impact_papers.py --citation_json_path 'citation_json/OSDI.json' --cite_per_year 30 --max_citation_threshold 500 > high_impact_papers/OSDI_high_impact_papers
+python3 print_high_impact_papers.py --citation_json_path 'citation_json/SOSP.json' --cite_per_year 30 --max_citation_threshold 500 > high_impact_papers/SOSP_high_impact_papers
+python3 print_high_impact_papers.py --citation_json_path 'citation_json/SIGMOD.json' --cite_per_year 30 --max_citation_threshold 500 > high_impact_papers/SIGMOD_high_impact_papers
 
 Input json format:
 {
